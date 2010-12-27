5936c9c8e18cb09ac77105f9f0ed8d9ebe68a47e
Kai Schlamp <schlamp@gmx.de>
2010-12-27 14:32:26 +0100

    Setup gem structure and test environment.

---
+++ spec/spec_helper.rb
@@ -0,0 +1,41 @@
+require 'rubygems'
+require 'bundler'
+require 'logger'
+require 'rspec'
+require 'active_record'
+require 'database_cleaner'
+
+$LOAD_PATH.unshift(File.dirname(__FILE__) + '/../lib')
+require 'make_voteable'
+
+ActiveRecord::Base.logger = Logger.new(File.dirname(__FILE__) + '/debug.log')
+ActiveRecord::Base.configurations = YAML::load_file(File.dirname(__FILE__) + '/database.yml')
+ActiveRecord::Base.establish_connection(ENV['DB'] || 'sqlite3')
+
+ActiveRecord::Base.silence do
+  ActiveRecord::Migration.verbose = false
+
+  load(File.dirname(__FILE__) + '/schema.rb')
+  load(File.dirname(__FILE__) + '/models.rb')
+end
+
+RSpec.configure do |config|
+  config.filter_run :focus => true
+  config.run_all_when_everything_filtered = true
+  config.filter_run_excluding :exclude => true
+
+  config.mock_with :rspec
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+end
